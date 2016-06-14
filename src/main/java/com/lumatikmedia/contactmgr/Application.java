5fb56e987fe60c9918e401c1a8f95796a666c0b8
mikejgulley <mgulley.mograph@gmail.com>
2016-06-14 16:10:42 -0400

    Course completed. Still need to implement Remove/Delete functionality.

--- src/main/java/com/lumatikmedia/contactmgr/Application.java
+++ src/main/java/com/lumatikmedia/contactmgr/Application.java
@@ -2,18 +2,23 @@ package com.lumatikmedia.contactmgr;
 
 import com.lumatikmedia.contactmgr.model.Contact;
 import com.lumatikmedia.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.jaxb.SourceType;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 /**
  * Created by mgulley on 6/7/2016.
  */
 public class Application {
     // Hold a reusable reference to a SessionFactory (since we need only one)
-    //private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         // Create a StandardServiceRegistry
@@ -26,6 +31,94 @@ public class Application {
                 .withEmail("me@meow.com")
                 .withPhone(5556667777L)
                 .build();
-        System.out.println(contact);
+
+        int id = save(contact);
+
+        // Display a list of contacts before the update
+        System.out.printf("%n%nBefore update%n%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+
+        // Get the persisted contact
+        Contact c = findContactById(id);
+
+        // Update the contact
+        c.setFirstName("Beatrix");
+
+        // Persist the changes
+        System.out.printf("%n%nUpdating...%n%n");
+        update(c);
+        System.out.printf("%n%nUpdate complete!%n%n");
+
+        // Display a list of contacts after the update
+        System.out.printf("%n%nAfter update%n%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+
+    }
+
+    private static Contact findContactById(int id) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Retrieve the persistent object (or null if not found)
+        Contact contact = session.get(Contact.class, id);
+
+        // Close the session
+        session.close();
+
+        // Return the object
+        return contact;
+    }
+
+    private static void update(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to update the contact
+        session.update(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts() {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Create the criteria object
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        // Get a list of Contact objects according to the Criteria object
+        List<Contact> contacts = criteria.list();
+
+        // Close the session
+        session.close();
+
+        return contacts;
+    }
+
+    private static int save(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use session to save the contact
+        int id = (int) session.save(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
+
+        return id;
     }
 }
